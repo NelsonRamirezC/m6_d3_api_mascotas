51b17292a95affa57ff5bf5b63d227db52fb9d07
Nelson Ramirez <xhyskoax@gmail.com>
2023-06-13 20:46:22 -0400

    Se agrega ejemplo log de uso de file System

---
+++ log.js
@@ -0,0 +1,6 @@
+//const fs = require("fs");
+
+//let data = fs.readFileSync(__dirname + "/logs.txt", "utf8");
+//fs.writeFileSync(__dirname + "/logs.txt", data + "\nlínea 3", "utf8");
+
+//fs.appendFileSync(__dirname + "/logs.txt", "\notra línea", "utf8");
